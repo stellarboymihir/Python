35a5e0672c6659eece3f943351eb5bdd714d56ad
stellarboymihir <stellarboymihir00@gmail.com>
2022-08-21 12:14:49 +0530

    Kaggle

---
+++ hello.py
@@ -0,0 +1,14 @@
+msg = "Hello World!";
+print(msg)
+
+spam_amount = 0;
+print(spam_amount)
+
+spam_amount += 4
+print(spam_amount)
+
+if(spam_amount > 0):
+    print("But I don't want any spam")
+
+viking_song = "Spam " * spam_amount
+print(viking_song)
