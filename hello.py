1e02856cea1719e3cc18eff560f02024e7941dc3
stellarboymihir <stellarboymihir00@gmail.com>
2022-08-21 12:46:54 +0530

    Updated

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
-msg = "Hello World!";
+msg = "Hello World!"
 print(msg)
 
-spam_amount = 0;
+spam_amount = 0
 print(spam_amount)
 
 spam_amount += 4
@@ -12,3 +12,39 @@ if(spam_amount > 0):
 
 viking_song = "Spam " * spam_amount
 print(viking_song)
+
+print(type(19.45))
+
+print(type(spam_amount))
+
+print(5/2)
+print(6/2)
+
+print(5//2)
+print(8//2)
+
+print(8-3+2)
+
+print(-3 + 4 *2)
+
+
+hat_ht = 25
+my_ht = 190
+
+total_ht = hat_ht + my_ht / 100
+print("Height in metres:- ", total_ht , "?")
+
+total_ht = (hat_ht + my_ht)/100
+print("Height in metres", total_ht, "?")
+
+
+print(min(7,3,8))
+print(max(6,2,7))
+
+print(abs(-45))
+print(abs(45))
+
+print(float(45))
+print(int(35))
+# They can even be called on Strings!
+print(int('805') + 46)
